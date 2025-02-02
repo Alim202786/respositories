b5a8162c010f41bc52c25e294e8a4678d726c309
Alim202786 <ibatulla.alimpc@gmail.com>
2025-02-03 00:03:48 +0500

    1

---
+++ 1lab/Data Types/1data.py
@@ -0,0 +1,11 @@
+"""
+Встроенные типы данных
+"""
+#Тип текста:	str
+#Числовые типы:	int, float, complex
+#Типы последовательности:	list, tuple, range
+#Тип отображения:	dict
+#Типы наборов:	set,frozenset
+#Тип логического значения:	bool
+#Двоичные типы:	bytes, bytearray, memoryview
+#Тип:	NoneType
