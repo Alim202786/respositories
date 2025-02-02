aa403f1ced6a6e887aa8257d0fdb54222950959e
Alim202786 <ibatulla.alimpc@gmail.com>
2025-02-03 00:00:43 +0500

    1

---
+++ 1lab/Variables/Variable Names/1var.name.py
@@ -0,0 +1,18 @@
+"""
+Имена переменных
+"""
+#Допустимые имена переменных
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+
+
+print(myvar)
+print(my_var)
+print(_my_var)
+print(myVar)
+print(MYVAR)
+print(myvar2)
