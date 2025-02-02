d164e19ca22710faba024be588868eafc8fcb265
Alim202786 <ibatulla.alimpc@gmail.com>
2025-02-03 00:04:28 +0500

    1

---
+++ 1lab/Numbers/1number.py
@@ -0,0 +1,21 @@
+"""
+Числа Python
+"""
+#Числа Python
+#int
+#float
+#complex
+#Переменные числовых типов создаются при присвоении им значения
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+
+
+
+x = 1
+y = 2.8
+z = 1j
+
+print(type(x))         #Чтобы проверить тип любого объекта в Python, используйте type() функцию
+print(type(y))
+print(type(z))
---
+++ 1lab/Numbers/5number.py
@@ -0,0 +1,21 @@
+"""
+Type Conversion
+"""
+#Вы можете преобразовать один тип в другой с помощью методов int(), float(), и complex()
+
+#convert from int to float:
+x = float(1)
+
+#convert from float to int:
+y = int(2.8)
+
+#convert from int to complex:
+z = complex(1)
+
+print(x)
+print(y)
+print(z)
+
+print(type(x))
+print(type(y))
+print(type(z))
