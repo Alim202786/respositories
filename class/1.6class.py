9a2d005c265fc54fd77214cb3430160843894a00
Alim202786 <ibatulla.alimpc@gmail.com>
2025-02-02 18:52:49 +0500

    Delete class directory

--- class/1.6class.py
+++
@@ -1,5 +0,0 @@
-prime = lambda n: n > 1 and all(n % i !=0 for i in range(2, int((n)**0.5) + 1))
-num = input()
-numbers = list(map(int, num.split()))
-prime_numbers =list(filter(prime, numbers))
-print("Prime numbers: ", prime_numbers)
