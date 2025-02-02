dd3c1cfa2a46b3fe10dae47e80ba4603e9cdaef6
Alim202786 <ibatulla.alimpc@gmail.com>
2025-02-02 20:05:44 +0500

    Delete Ex directory

--- Ex/Function/2Ex.function.py
+++
@@ -1,7 +0,0 @@
-def my_function(fname):         #Аргументы указываются после имени функции внутри ()
-    print(fname + " JO")
-
-#Пример использование
-my_function("Smit")             
-my_function("GO")                   
-my_function("JO")
--- Ex/Function/7Ex.function.py
+++
@@ -1,8 +0,0 @@
-def my_function(country = "Kazakstan"):               #Мы даем country значение поумолчанию
-  print("I am from " + country)
-
-#Пример использование
-my_function("Sweden")
-my_function("India")
-my_function()                                         #Здесь по умолчанию будет Kazakstan даже если ничего не писать
-my_function("Brazil")
