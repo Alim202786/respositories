6e72779d122a3c155aeda7858b2156b2c488b54e
Alim202786 <ibatulla.alimpc@gmail.com>
2025-02-03 00:08:53 +0500

    1

---
+++ 1lab/Casting/1casting.py
@@ -0,0 +1,34 @@
+"""
+Specify a Variable Type
+"""
+#int() — создает целое число из целочисленного литерала, литерала с плавающей точкой
+#float() — создает число с плавающей точкой из целочисленного литерала, литерала с плавающей точкой или строкового литерала
+#str() — создает строку из самых разных типов данных, включая строки, целочисленные литералы и литералы с плавающей точкой.
+
+#1
+x = int(1)   # x will be 1
+y = int(2.8) # y will be 2
+z = int("3") # z will be 3
+print(x)
+print(y)
+print(z)
+
+
+#2
+x = float(1)     # x will be 1.0
+y = float(2.8)   # y will be 2.8
+z = float("3")   # z will be 3.0
+w = float("4.2") # w will be 4.2
+print(x)
+print(y)
+print(z)
+print(w)
+
+
+#3
+x = str("s1") # x will be 's1'
+y = str(2)    # y will be '2'
+z = str(3.0)  # z will be '3.0'
+print(x)
+print(y)
+print(z)
