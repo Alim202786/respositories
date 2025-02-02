4e7d06f19f9b6887ccf42c44819536a8fa0121f5
Alim202786 <ibatulla.alimpc@gmail.com>
2025-02-02 23:58:02 +0500

    1

---
+++ 1lab/Comments/1comment.py
@@ -0,0 +1,15 @@
+"""
+Создание комментария
+""" 
+#Комментарии начинаются с #, и Python их игнорирует:
+
+#1
+#This is a comment.
+print("Hello, World!")
+
+#2
+print("Hello, World!") #This is a comment.
+
+#3
+#print("Hello, World!")
+print("Cheers, Mate!")
---
+++ 1lab/Comments/2comment.py
@@ -0,0 +1,17 @@
+"""
+Многострочные комментарии
+""" 
+#В Python на самом деле нет синтаксиса для многострочных комментариев
+
+#This is a comment
+#written in
+#more than just one line
+print("Hello, World!") 
+
+
+"""
+This is a comment
+written in
+more than just one line
+"""
+print("Hello, World!") 
